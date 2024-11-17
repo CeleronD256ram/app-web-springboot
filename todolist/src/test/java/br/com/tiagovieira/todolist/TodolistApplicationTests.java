8421ab33b0b743b33d69074163ad8270e3af8f35
Tiago Vieira <celerond256@outlook.com>
2024-11-17 20:17:36 -0300

    Projeto estruturado

---
+++ todolist/src/test/java/br/com/tiagovieira/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tiagovieira.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
